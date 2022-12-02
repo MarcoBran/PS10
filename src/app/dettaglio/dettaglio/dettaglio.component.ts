1941e007ce6d1858758ebcfe6d34f971c5bc5044
Marco Brandi <114394974+MarcoBran@users.noreply.github.com>
2022-12-02 12:01:00 +0100

    Add files via upload

---
+++ src/app/dettaglio/dettaglio/dettaglio.component.ts
@@ -0,0 +1,16 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Post } from 'src/app/post';
+
+@Component({
+  selector: 'app-dettaglio',
+  templateUrl: './dettaglio.component.html',
+  styleUrls: ['./dettaglio.component.scss']
+})
+export class DettaglioComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
